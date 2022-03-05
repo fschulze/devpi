770afe887ea2d73fc4feedae836b511c244339e8
Florian Schulze <mail@florian-schulze.net>
2022-03-05 12:21:01 +0100

    Add Cython 3.0.0a11 for Python 3.11 nightly.

--- .ci/upload-packages.sh
+++ .ci/upload-packages.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -xe -o nounset
 yes yes | devpi index --delete "${DEVPI_INDEX}" || true
-devpi index -c "${DEVPI_INDEX}" bases=root/pypi
+devpi index -c "${DEVPI_INDEX}" bases=root/pypi mirror_whitelist=Cython
 devpi use "${DEVPI_INDEX}"
 devpi push --index root/pypi devpi-server==4.0.0 "${DEVPI_INDEX}"
 devpi push --index root/pypi devpi-server==5.2.0 "${DEVPI_INDEX}"
+devpi push --index fschulze/dev Cython==3.0.0a11 "${DEVPI_INDEX}"
 pushd common
 yes | towncrier
 devpi upload
