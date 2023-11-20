8fa74232b99ca1f11f4db9adbf1ab429c10bafcc
Florian Schulze <mail@florian-schulze.net>
2023-11-20 11:41:05 +0100

    Add explicit 'MutableFileEntry'.

--- server/devpi_server/filestore.py
+++ server/devpi_server/filestore.py
@@ -5,6 +5,7 @@ for all indexes.
 """
 import hashlib
 import mimetypes
+from .readonly import get_mutable_deepcopy
 from wsgiref.handlers import format_date_time
 import re
 from devpi_common.metadata import splitbasename
@@ -328,7 +329,7 @@ class FileStore:
 
     def maplink(self, link, user, index, project):
         key = key_from_link(self.keyfs, link, user, index)
-        entry = FileEntry(key, readonly=False)
+        entry = MutableFileEntry(key)
         entry.url = link.geturl_nofragment().url
         entry.hash_spec = unicode_if_bytes(link.hash_spec)
         entry.project = project
@@ -352,14 +353,14 @@ class FileStore:
             return None
         return key
 
-    def get_file_entry(self, relpath, readonly=True):
+    def get_file_entry(self, relpath):
         key = self.get_key_from_relpath(relpath)
         if key is None:
             return None
-        return self.get_file_entry_from_key(key, readonly=readonly)
+        return FileEntry(key)
 
-    def get_file_entry_from_key(self, key, meta=_nodefault, readonly=True):
-        return FileEntry(key, meta=meta, readonly=readonly)
+    def get_file_entry_from_key(self, key, meta=_nodefault):
+        return MutableFileEntry(key, meta=meta)
 
     def store(self, user, index, basename, content_or_file, *, dir_hash_spec=None, hashes=None):
         # dir_hash_spec is set for toxresult files
@@ -369,7 +370,7 @@ class FileStore:
         key = self.keyfs.STAGEFILE(
             user=user, index=index,
             hashdir_a=hashdir_a, hashdir_b=hashdir_b, filename=basename)
-        entry = FileEntry(key, readonly=False)
+        entry = MutableFileEntry(key)
         entry.file_set_content(content_or_file, hashes=hashes)
         return entry
 
@@ -394,8 +395,9 @@ class BadGateway(Exception):
         self.url = url
 
 
-class FileEntry(object):
-    __slots__ = ('_meta', '_storepath', 'basename', 'key', 'readonly', 'relpath')
+class BaseFileEntry:
+    __slots__ = ('_meta', '_storepath', 'basename', 'key', 'relpath')
+
     BadGateway = BadGateway
     _hash_spec = metaprop("hash_spec")  # e.g. "md5=120938012"
     last_modified = metaprop("last_modified")
@@ -403,11 +405,10 @@ class FileEntry(object):
     project = metaprop("project")
     version = metaprop("version")
 
-    def __init__(self, key, meta=_nodefault, readonly=True):
+    def __init__(self, key, meta=_nodefault):
         self.key = key
         self.relpath = key.relpath
         self.basename = self.relpath.split("/")[-1]
-        self.readonly = readonly
         self._storepath = "/".join(("+files", str(self.relpath)))
         self._meta = _nodefault
         if meta is not _nodefault:
@@ -510,9 +511,7 @@ class FileEntry(object):
 
     @property
     def meta(self):
-        if self._meta is _nodefault:
-            self._meta = self.key.get(readonly=self.readonly)
-        return self._meta
+        raise NotImplementedError
 
     def file_exists(self):
         return self.tx.conn.io_file_exists(self._storepath)
@@ -524,7 +523,7 @@ class FileEntry(object):
         return self.tx.conn.io_file_size(self._storepath)
 
     def __repr__(self):
-        return f"<FileEntry {self.key!r}>"
+        return f"<{self.__class__.__name__} {self.key!r}>"
 
     def file_new_open(self):
         return self.tx.conn.io_file_new_open(self._storepath)
@@ -602,6 +601,30 @@ class FileEntry(object):
             return errors.get(
                 self.best_available_hash_type, next(iter(errors.values())))['msg']
         return None
+
+
+class FileEntry(BaseFileEntry):
+    @property
+    def meta(self):
+        if self._meta is _nodefault:
+            self._meta = self.key.get()
+        return self._meta
+
+    @property
+    def readonly(self):
+        return True
+
+
+class MutableFileEntry(BaseFileEntry):
+    @property
+    def meta(self):
+        if self._meta is _nodefault:
+            self._meta = get_mutable_deepcopy(self.key.get())
+        return self._meta
+
+    @property
+    def readonly(self):
+        return False
 
 
 def get_checksum_error(content_or_hash, relpath, hash_spec):
