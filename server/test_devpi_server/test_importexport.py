7ca11453c7fec04713495028bd71d3287505656b
Florian Schulze <mail@florian-schulze.net>
2023-11-23 08:43:14 +0100

    Remove special doczip handling on export.
    This enables hard-links for doczips.

--- server/test_devpi_server/test_importexport.py
+++ server/test_devpi_server/test_importexport.py
@@ -952,7 +952,7 @@ class TestImportExport:
         assert impexp.exportdir.joinpath(
             'dataindex.json').stat().st_nlink == 1
         assert impexp.exportdir.joinpath(
-            'user1', 'dev', 'he_llo-1.0.doc.zip').stat().st_nlink == 2
+            'user1', 'dev', 'he-llo-1.0.doc.zip').stat().st_nlink == 2
         assert impexp.exportdir.joinpath(
             'user1', 'dev', 'he-llo', '1.0', 'he-llo-1.0.tar.gz').stat().st_nlink == 2
 
@@ -990,7 +990,7 @@ class TestImportExport:
         assert impexp.exportdir.joinpath(
             'dataindex.json').stat().st_nlink == 1
         assert impexp.exportdir.joinpath(
-            'user1', 'dev', 'he_llo-1.0.doc.zip').stat().st_nlink == 1
+            'user1', 'dev', 'he-llo-1.0.doc.zip').stat().st_nlink == 1
         assert impexp.exportdir.joinpath(
             'user1', 'dev', 'he-llo', '1.0', 'he-llo-1.0.tar.gz').stat().st_nlink == 1
 
@@ -1005,6 +1005,7 @@ class TestImportExport:
             (link,) = stage.get_releaselinks("he_llo")
             assert link.entry.file_get_content() == b'content'
             # assert os.stat(link.entry.file_os_path()).st_nlink == 2
+            assert os.stat(link.entry.file_os_path()).st_nlink == 2
             doczip = stage.get_doczip("he_llo", "1.0")
             archive = Archive(BytesIO(doczip))
             assert 'index.html' in archive.namelist()
@@ -1014,7 +1015,7 @@ class TestImportExport:
         assert impexp.exportdir.joinpath(
             'dataindex.json').stat().st_nlink == 1
         assert impexp.exportdir.joinpath(
-            'user1', 'dev', 'he_llo-1.0.doc.zip').stat().st_nlink == 2
+            'user1', 'dev', 'he-llo-1.0.doc.zip').stat().st_nlink == 2
         assert impexp.exportdir.joinpath(
             'user1', 'dev', 'he-llo', '1.0', 'he-llo-1.0.tar.gz').stat().st_nlink == 2
 
