3c6189899e33933d8e54c7672e13ea5536bfe1aa
Florian Schulze <mail@florian-schulze.net>
2023-11-25 14:20:54 +0100

    Add io_file attribute to transaction to allow connection independent implementations.

---
+++ server/devpi_server/filestore_db.py
@@ -0,0 +1,44 @@
+from .interfaces import IDBIOFileConnection
+from .interfaces import IIOFile
+from zope.interface import implementer
+from zope.interface.verify import verifyObject
+
+
+@implementer(IIOFile)
+class DBIOFile:
+    def __init__(self, conn):
+        conn = IDBIOFileConnection(conn)
+        verifyObject(IDBIOFileConnection, conn)
+        self._dirty_files = conn.dirty_files
+        self.commit = conn.commit_files_without_increasing_serial
+        self.delete = conn.io_file_delete
+        self.exists = conn.io_file_exists
+        self.get_content = conn.io_file_get
+        self.new_open = conn.io_file_new_open
+        self.open_read = conn.io_file_open
+        self.os_path = conn.io_file_os_path
+        self.perform_crash_recovery = getattr(conn.storage, "perform_crash_recovery", self._perform_crash_recovery)
+        self.rollback = getattr(conn, "_drop_dirty_files", self._rollback)
+        self.set_content = conn.io_file_set
+        self.size = conn.io_file_size
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, cls, val, tb):
+        if cls is not None:
+            self.rollback()
+            return False
+        return True
+
+    def get_rel_renames(self):
+        return []
+
+    def is_dirty(self):
+        return bool(self._dirty_files)
+
+    def _perform_crash_recovery(self):
+        pass
+
+    def _rollback(self):
+        pass
