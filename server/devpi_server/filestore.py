f90522faa687f47ef57942849631e8470c3caff1
Florian Schulze <mail@florian-schulze.net>
2024-04-17 17:50:42 +0200

    Add ref_hash_spec to more easily allow future changes of the used hash function.

--- server/devpi_server/filestore.py
+++ server/devpi_server/filestore.py
@@ -369,11 +369,11 @@ class FileStore:
     def get_file_entry_from_key(self, key, meta=_nodefault):
         return MutableFileEntry(key, meta=meta)
 
-    def store(self, user, index, basename, content_or_file, *, dir_hash_spec=None, hashes):
-        # dir_hash_spec is set for toxresult files
-        if dir_hash_spec is None:
-            dir_hash_spec = hashes.get_default_spec()
-        hashdir_a, hashdir_b = make_splitdir(dir_hash_spec)
+    def store(self, user, index, basename, content_or_file, *, ref_hash_spec=None, hashes):
+        # ref_hash_spec is set for toxresult files to store them alongside the tested release
+        if ref_hash_spec is None:
+            ref_hash_spec = hashes.get_default_spec()
+        hashdir_a, hashdir_b = make_splitdir(ref_hash_spec)
         key = self.keyfs.STAGEFILE(
             user=user, index=index,
             hashdir_a=hashdir_a, hashdir_b=hashdir_b, filename=basename)
@@ -479,6 +479,10 @@ class BaseFileEntry:
             DeprecationWarning,
             stacklevel=2)
         return self.best_available_hash_type
+
+    @property
+    def ref_hash_spec(self):
+        return self.hashes.get_default_spec()
 
     def file_get_checksum(self, hash_type):
         warnings.warn(
