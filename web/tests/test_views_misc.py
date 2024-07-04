019d6e4f8953930e366fdb55156b2e60cc5f901b
Florian Schulze <mail@florian-schulze.net>
2024-07-04 13:34:40 +0200

    Move status macro to separate header_status template in new main_header macro group.

--- web/tests/test_views_misc.py
+++ web/tests/test_views_misc.py
@@ -350,9 +350,7 @@ class TestStatusView:
         assert result['msgs'] == msgs
 
     def test_status_macros_nothing(self, dummyrequest, plugin, statusview):
-        from devpi_web.main import status_info
         plugin.results = [[]]
-        dummyrequest.status_info = status_info(dummyrequest)
         result = statusview(None, dummyrequest)
         html = BeautifulSoup(result.body, 'html.parser')
         assert html.select('.statusbadge')[0].text.strip() == 'ok'
@@ -360,9 +358,7 @@ class TestStatusView:
         assert html.select('#serverstatus') == []
 
     def test_status_macros_warn(self, dummyrequest, plugin, statusview):
-        from devpi_web.main import status_info
         plugin.results = [[dict(status="warn", msg="Foo")]]
-        dummyrequest.status_info = status_info(dummyrequest)
         result = statusview(None, dummyrequest)
         html = BeautifulSoup(result.body, 'html.parser')
         assert html.select('.statusbadge')[0].text.strip() == 'degraded'
@@ -370,9 +366,7 @@ class TestStatusView:
         assert html.select('#serverstatus') == []
 
     def test_status_macros_fatal(self, dummyrequest, plugin, statusview):
-        from devpi_web.main import status_info
         plugin.results = [[dict(status="fatal", msg="Foo")]]
-        dummyrequest.status_info = status_info(dummyrequest)
         result = statusview(None, dummyrequest)
         html = BeautifulSoup(result.body, 'html.parser')
         assert html.select('.statusbadge')[0].text.strip() == 'fatal'
@@ -383,9 +377,7 @@ class TestStatusView:
         [dict(status="warn", msg="Bar"), dict(status="fatal", msg="Foo")],
         [dict(status="fatal", msg="Foo"), dict(status="warn", msg="Bar")]])
     def test_status_macros_mixed(self, dummyrequest, plugin, statusview, msgs):
-        from devpi_web.main import status_info
         plugin.results = [msgs]
-        dummyrequest.status_info = status_info(dummyrequest)
         result = statusview(None, dummyrequest)
         html = BeautifulSoup(result.body, 'html.parser')
         assert html.select('.statusbadge')[0].text.strip() == 'fatal'
