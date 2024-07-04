6d90d205a1b28626451e5624969fa7c1fb72590c
Florian Schulze <mail@florian-schulze.net>
2024-07-04 12:57:36 +0200

    WIP Move logo macro to separate logo template.

--- web/devpi_web/macros.py
+++ web/devpi_web/macros.py
@@ -5,3 +5,8 @@ from .macroregistry import macro_config
 def footer_versions(request):
     return dict(
         version_infos=request.registry.get('devpi_version_info'))
+
+
+@macro_config(template='templates/logo.pt')
+def logo(request):
+    return dict()
