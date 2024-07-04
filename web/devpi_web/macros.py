cd7d5db5420bb6d397f8884b892e3a905b2281ce
Florian Schulze <mail@florian-schulze.net>
2024-07-04 13:34:04 +0200

    Move query_doc macro to separate query_doc template.

--- web/devpi_web/macros.py
+++ web/devpi_web/macros.py
@@ -18,6 +18,15 @@ def logo(request):
     return dict()
 
 
+@macro_config(template='templates/query_docs.pt')
+def query_doc(request):
+    query_docs_html = None
+    search_index = request.registry.get('search_index')
+    if search_index is not None:
+        query_docs_html = search_index.get_query_parser_html_help()
+    return dict(query_docs_html=query_docs_html)
+
+
 @macro_config(template='templates/render_group.pt')
 def render_group(request, group_name):
     return dict(
