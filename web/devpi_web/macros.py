901e13db5a6092bf074f02f89d80180539b1ff5b
Florian Schulze <mail@florian-schulze.net>
2024-07-04 12:49:44 +0200

    Move versions macro to separate footer_versions template.

---
+++ web/devpi_web/macros.py
@@ -0,0 +1,7 @@
+from .macroregistry import macro_config
+
+
+@macro_config(template='templates/footer_versions.pt', groups='main_footer')
+def footer_versions(request):
+    return dict(
+        version_infos=request.registry.get('devpi_version_info'))
--- web/tests/test_views_misc.py
+++ web/tests/test_views_misc.py
@@ -293,6 +293,7 @@ class TestStatusView:
         pyramidconfig.include('devpi_web.macroregistry')
         pyramidconfig.add_static_view('+static', 'devpi_web:static')
         pyramidconfig.add_route("/+status", "/+status")
+        pyramidconfig.scan('devpi_web.macros')
         pyramidconfig.scan('devpi_web.views', ignore=lambda n: 'statusview' not in n)
         dummyrequest.macros = macros(dummyrequest)
         return _getViewCallable(
@@ -428,6 +429,7 @@ class TestReplicaStatusView:
         pyramidconfig.include('devpi_web.macroregistry')
         pyramidconfig.add_static_view('+static', 'devpi_web:static')
         pyramidconfig.add_route("/+status", "/+status")
+        pyramidconfig.scan('devpi_web.macros')
         pyramidconfig.scan('devpi_web.views', ignore=lambda n: 'statusview' not in n)
         dummyrequest.macros = macros(dummyrequest)
         return _getViewCallable(
