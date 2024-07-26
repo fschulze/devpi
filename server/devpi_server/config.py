5d4836ea5a557ae6a41f3b246b0e567b2d828a68
Florian Schulze <mail@florian-schulze.net>
2024-07-26 16:45:50 +0200

    WIP de-duplication
    WIP need to fix test_dirty_files_removed_on_rollback for de-duplication

--- server/devpi_server/config.py
+++ server/devpi_server/config.py
@@ -593,10 +593,13 @@ def get_io_file_factory(storage_info):
         from .filestore_db import DBIOFile
         verifyClass(IIOFile, DBIOFile)
         _io_file_factory = DBIOFile
-    else:
+    elif True:
         from .filestore_fs import FSIOFile
         verifyClass(IIOFile, FSIOFile)
         _io_file_factory = FSIOFile
+    else:
+        from .filestore_hash import HashIOFile  # type: ignore[unreachable]
+        _io_file_factory = HashIOFile
     settings = storage_info.settings
 
     def io_file_factory(conn):
