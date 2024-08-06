ecfacd2a018188b716fa6c5e8ba83c805bada4e1
Florian Schulze <mail@florian-schulze.net>
2024-08-06 09:36:03 +0200

    Remove add_key.

--- server/devpi_server/keyfs.py
+++ server/devpi_server/keyfs.py
@@ -31,7 +31,6 @@ from pathlib import Path
 import contextlib
 import errno
 import time
-import warnings
 
 
 class KeyfsTimeoutError(TimeoutError):
@@ -353,17 +352,6 @@ class KeyFS:
     def tx(self):
         return self._threadlocal.tx
 
-    def add_key(self, name, path, type):
-        warnings.warn(
-            "The add_key method is deprecated, "
-            "use register*_key instead",
-            DeprecationWarning,
-            stacklevel=2)
-        assert isinstance(path, str)
-        key = self.register_named_key(name, path, None, type)
-        self._storage.add_key(key)
-        return key
-
     def register_key(self, key):
         allowed_chars = frozenset('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_')
         key_name = key.key_name
@@ -372,8 +360,7 @@ class KeyFS:
             raise ValueError("Duplicate registration for key named '%s'" % key_name)
         self._keys[key_name] = key
         setattr(self, key_name, key)
-        if hasattr(self._storage, 'register_key'):
-            self._storage.register_key(key)
+        self._storage.register_key(key)
         return key
 
     def register_located_key(self, key_name, location, name, key_type):
