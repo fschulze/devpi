687535c9862adf2b50a68a4edbe2e945ef71c74e
Florian Schulze <mail@florian-schulze.net>
2024-08-06 21:18:38 +0200

    Replace get_changes with iter_changes_at.

---
+++ server/devpi_server/compat.py
@@ -0,0 +1,11 @@
+try:
+    from inspect import get_annotations
+except ImportError:
+    def get_annotations(obj):
+        if hasattr(obj, '__annotations__'):
+            return obj.__annotations__
+        if hasattr(obj, '__call__'):  # noqa: B004 - we actually need to check for the attribute here
+            return get_annotations(obj.__call__)
+        if hasattr(obj, '__func__'):
+            return get_annotations(obj.__func__)
+        return {}
