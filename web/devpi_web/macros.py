b58df325dd16dc0cd24d46b398846a20c9036010
Florian Schulze <mail@florian-schulze.net>
2025-05-07 19:51:09 +0200

    Move head macro to separate head template.

--- web/devpi_web/macros.py
+++ web/devpi_web/macros.py
@@ -18,6 +18,11 @@ def footer_versions(request):
         version_infos=request.registry.get('devpi_version_info'))
 
 
+@macro_config(template='templates/head.pt')
+def head(request):  # noqa: ARG001
+    return dict()
+
+
 @macro_config(template='templates/header_status.pt', groups='main_header', legacy_name='status')
 def header_status(request):
     return dict(status_info=request.status_info)
