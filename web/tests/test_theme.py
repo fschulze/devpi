ac0352bf1b9cf5358ac6d82000f18a7bb90c564c
Florian Schulze <mail@florian-schulze.net>
2025-05-07 14:45:44 +0200

    Use pytest.mark to inject theme files directly into fixture creation.

--- web/tests/test_theme.py
+++ web/tests/test_theme.py
@@ -5,43 +5,53 @@ pytestmark = [pytest.mark.notransaction]
 
 
 @pytest.fixture
-def themedir(tmpdir):
+def themedir(request, tmpdir):
+    marker = request.node.get_closest_marker("theme_files")
+    files = {} if marker is None else marker.args[0]
     path = tmpdir.join('theme')
     path.ensure_dir()
     path.join('static').ensure_dir()
     path.join('templates').ensure_dir()
+    for filepath, content in files.items():
+        path.join(*filepath).write(content)
     return path
 
 
 @pytest.fixture
-def xom(request, xom, themedir):
+def xom(xom, themedir):
     xom.config.args.theme = themedir.strpath
     return xom
 
 
-def test_macro_overwrite(testapp, themedir):
-    from devpi_web import __version__
-    themedir.join('templates', 'macros.pt').write("""
+@pytest.mark.usefixtures("themedir")
+@pytest.mark.theme_files({
+    ('templates', 'macros.pt'): """
 <metal:head define-macro="headcss" use-macro="request.macros['original-headcss']">
     <metal:mycss fill-slot="headcss">
         <link rel="stylesheet" type="text/css" href="${request.theme_static_url('style.css')}" />
     </metal:mycss>
 </metal:head>
-    """)
+    """})
+def test_macro_overwrite(testapp):
+    from devpi_web import __version__
     r = testapp.get('/')
     styles = [x.attrs.get('href') for x in r.html.find_all('link')]
     assert 'http://localhost/+static-%s/style.css' % __version__ in styles
     assert 'http://localhost/+theme-static-%s/style.css' % __version__ in styles
 
 
-def test_template_overwrite(testapp, themedir):
-    themedir.join('templates', 'root.pt').write("Foo Template!")
+@pytest.mark.usefixtures("themedir")
+@pytest.mark.theme_files({
+    ('templates', 'root.pt'): "Foo Template!"})
+def test_template_overwrite(testapp):
     r = testapp.get('/')
     assert r.text == 'Foo Template!'
 
 
-def test_theme_style(testapp, themedir):
+@pytest.mark.usefixtures("themedir")
+@pytest.mark.theme_files({
+    ('static', 'style.css'): "Foo Style!"})
+def test_theme_style(testapp):
     from devpi_web import __version__
-    themedir.join('static', 'style.css').write("Foo Style!")
-    r = testapp.get('/+theme-static-%s/style.css' % __version__)
+    r = testapp.get(f'/+theme-static-{__version__}/style.css')
     assert r.text == 'Foo Style!'
