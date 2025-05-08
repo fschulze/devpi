d0a4437bf35b57361f90364cc821685cc68bdf62
Florian Schulze <mail@florian-schulze.net>
2025-05-08 14:50:06 +0200

    Move search macro to separate header_search template.

--- web/devpi_web/macros.py
+++ web/devpi_web/macros.py
@@ -1,3 +1,4 @@
+from .macroregistry import GroupDef
 from .macroregistry import macro_config
 import os
 
@@ -23,6 +24,14 @@ def head(request):  # noqa: ARG001
     return dict()
 
 
+@macro_config(
+    template='templates/header_search.pt',
+    groups=GroupDef('main_header_top', after='logo'),
+    legacy_name='search')
+def header_search(request):  # noqa: ARG001
+    return dict()
+
+
 @macro_config(template='templates/header_status.pt', groups='main_header', legacy_name='status')
 def header_status(request):
     return dict(status_info=request.status_info)
@@ -45,6 +54,7 @@ def html_head_css(request):
 def html_head_scripts(request):
     request.add_static_script('devpi_web:static/jquery-3.6.0.min.js')
     request.add_static_script('devpi_web:static/common.js')
+    request.add_static_script('devpi_web:static/search.js')
     scripts = request.environ.setdefault('devpiweb.head_scripts', [])
     return dict(scripts=scripts)
 
