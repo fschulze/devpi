1802906b16ccdaca43ac2fd49f517002d0ee8cbd
Florian Schulze <mail@florian-schulze.net>
2025-09-09 09:28:04 +0200

    Move remote file iteration functions to mirror.py.

--- server/test_devpi_server/test_replica.py
+++ server/test_devpi_server/test_replica.py
@@ -8,13 +8,13 @@ from devpi_server.keyfs_types import RelPath
 from devpi_server.log import thread_pop_log
 from devpi_server.log import thread_push_log
 from devpi_server.log import threadlog
+from devpi_server.mirror import iter_remote_file_replica
 from devpi_server.replica import H_EXPECTED_PRIMARY_ID
 from devpi_server.replica import H_PRIMARY_UUID
 from devpi_server.replica import H_REPLICA_OUTSIDE_URL
 from devpi_server.replica import H_REPLICA_UUID
 from devpi_server.replica import PrimaryChangelogRequest
 from devpi_server.replica import proxy_view_to_primary
-from devpi_server.views import iter_remote_file_replica
 from pyramid.httpexceptions import HTTPNotFound
 import os
 import pytest
