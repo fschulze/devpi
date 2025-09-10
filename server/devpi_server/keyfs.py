2e43beb5749906e447eb3cb3d6a93530519e8ce0
Florian Schulze <mail@florian-schulze.net>
2025-09-10 21:05:11 +0200

    Split get_last_serial_and_value_at to get rid of raise_on_error parameter.

--- server/devpi_server/keyfs.py
+++ server/devpi_server/keyfs.py
@@ -816,46 +816,20 @@ class Transaction:
             yield (data.last_serial, data.value)
             last_serial = data.back_serial
 
-    @overload
     def get_last_serial_and_value_at(
         self,
         typedkey: LocatedKey,
         at_serial: int,
-        *,
-        raise_on_error: Literal[False],
-    ) -> tuple[int, KeyFSTypesRO | None] | None:
-        pass
-
-    @overload
-    def get_last_serial_and_value_at(
-        self,
-        typedkey: LocatedKey,
-        at_serial: int,
-        *,
-        raise_on_error: Literal[True] = True,
-    ) -> tuple[int, KeyFSTypesRO | None]:
-        pass
-
-    def get_last_serial_and_value_at(
-        self,
-        typedkey: LocatedKey,
-        at_serial: int,
-        *,
-        raise_on_error: bool = True,
     ) -> tuple[int, KeyFSTypesRO | None] | None:
         relpath = typedkey.relpath
         try:
             data = self.conn.get_relpath_at(relpath, at_serial)
         except KeyError:
-            if not raise_on_error:
-                return None
-            raise
-        if data.value is None and raise_on_error:
-            raise KeyError(relpath)  # was deleted
+            return None
         return (data.last_serial, data.value)
 
     def get_value_at(self, typedkey: LocatedKey, at_serial: int) -> KeyFSTypesRO | None:
-        (last_serial, val) = self.get_last_serial_and_value_at(typedkey, at_serial)
+        (_last_serial, val) = self.last_serial_and_value_at(typedkey, at_serial)
         return val
 
     def last_serial(self, typedkey: LocatedKey) -> int:
@@ -864,6 +838,13 @@ class Transaction:
         (last_serial, _val) = self.get_original(typedkey)
         return last_serial
 
+    def last_serial_and_value_at(self, typedkey, at_serial):
+        relpath = typedkey.relpath
+        data = self.conn.get_relpath_at(relpath, at_serial)
+        if data.value is None:
+            raise KeyError(relpath)  # was deleted
+        return (data.last_serial, data.value)
+
     def is_dirty(self, typedkey):
         return typedkey in self.dirty
 
@@ -871,8 +852,7 @@ class Transaction:
         """ Return original value from start of transaction,
             without changes from current transaction."""
         if typedkey not in self._original:
-            tup = self.get_last_serial_and_value_at(
-                typedkey, self.at_serial, raise_on_error=False)
+            tup = self.get_last_serial_and_value_at(typedkey, self.at_serial)
             val: Absent | Deleted | KeyFSTypesRO | None
             if tup is None:
                 serial = -1
--- server/devpi_server/model.py
+++ server/devpi_server/model.py
@@ -1489,11 +1489,7 @@ class PrivateStage(BaseStage):
         tx = self.keyfs.tx
         if at_serial is None:
             at_serial = tx.at_serial
-        info = tx.get_last_serial_and_value_at(
-            self.key_projects,
-            at_serial,
-            raise_on_error=False,
-        )
+        info = tx.get_last_serial_and_value_at(self.key_projects, at_serial)
         if info is None:
             # never existed
             return -1
@@ -1502,9 +1498,7 @@ class PrivateStage(BaseStage):
             # the whole index was deleted
             return -1
         info = tx.get_last_serial_and_value_at(
-            self.key_projversions(project),
-            at_serial,
-            raise_on_error=False,
+            self.key_projversions(project), at_serial
         )
         if info is None:
             if project in projects:
@@ -1520,7 +1514,7 @@ class PrivateStage(BaseStage):
             return last_serial
         for version in versions:
             info = tx.get_last_serial_and_value_at(
-                self.key_projversion(project, version), at_serial, raise_on_error=False
+                self.key_projversion(project, version), at_serial
             )
             if info is None:
                 continue
@@ -1529,9 +1523,7 @@ class PrivateStage(BaseStage):
             if last_serial >= at_serial:
                 return last_serial
             info = tx.get_last_serial_and_value_at(
-                self.key_versionfilelist(project, version),
-                at_serial,
-                raise_on_error=False,
+                self.key_versionfilelist(project, version), at_serial
             )
             if info is None:
                 continue
@@ -1541,9 +1533,7 @@ class PrivateStage(BaseStage):
                 return last_serial
             for filename in versionfiles_info:
                 info = tx.get_last_serial_and_value_at(
-                    self.key_versionfile(project, version, filename),
-                    at_serial,
-                    raise_on_error=False,
+                    self.key_versionfile(project, version, filename), at_serial
                 )
                 if info is None:
                     continue
@@ -1693,21 +1683,23 @@ class PrivateStage(BaseStage):
         if at_serial is None:
             at_serial = tx.at_serial
         try:
-            (last_serial, projects) = tx.get_last_serial_and_value_at(
-                self.key_projects, at_serial)
+            (last_serial, projects) = tx.last_serial_and_value_at(
+                self.key_projects, at_serial
+            )
         except KeyError:
             last_serial = -1
             projects = ()
         if last_serial >= at_serial:
             return last_serial
         for project in projects:
-            (versions_serial, versions) = tx.get_last_serial_and_value_at(
-                self.key_projversions(project), at_serial)
+            (versions_serial, versions) = tx.last_serial_and_value_at(
+                self.key_projversions(project), at_serial
+            )
             last_serial = max(last_serial, versions_serial)
             if last_serial >= at_serial:
                 return last_serial
             for version in versions:
-                (version_serial, _version_value) = tx.get_last_serial_and_value_at(
+                (version_serial, _version_value) = tx.last_serial_and_value_at(
                     self.key_projversion(project, version), at_serial
                 )
                 last_serial = max(last_serial, version_serial)
@@ -1715,7 +1707,7 @@ class PrivateStage(BaseStage):
                     return last_serial
                 try:
                     (versionfiles_serial, versionfilenames) = (
-                        tx.get_last_serial_and_value_at(
+                        tx.last_serial_and_value_at(
                             self.key_versionfilelist(project, version), at_serial
                         )
                     )
@@ -1727,7 +1719,7 @@ class PrivateStage(BaseStage):
                 for filename in versionfilenames:
                     try:
                         (versionfile_serial, _versionfile_info) = (
-                            tx.get_last_serial_and_value_at(
+                            tx.last_serial_and_value_at(
                                 self.key_versionfile(project, version, filename),
                                 at_serial,
                             )
@@ -1739,7 +1731,7 @@ class PrivateStage(BaseStage):
                         return last_serial
         # no project uploaded yet
         index_key = self.key_index
-        (index_serial, _index_config) = tx.get_last_serial_and_value_at(
+        (index_serial, _index_config) = tx.last_serial_and_value_at(
             index_key, at_serial
         )
         if last_serial >= index_serial:
