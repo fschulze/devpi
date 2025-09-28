bce39199b465d29728078aa8607140f1a1693992
Florian Schulze <mail@florian-schulze.net>
2025-09-28 08:48:03 +0200

    Add MirrorLinks helper class.
    This will allow caching and other conveniences.

--- server/devpi_server/importexport.py
+++ server/devpi_server/importexport.py
@@ -623,7 +623,8 @@ class Importer:
                     url, stage.username, stage.index, project)
                 entry.file_set_content(
                     f, hashes=hashes, last_modified=mapping["last_modified"])
-                (_, links_with_data, serial, etag) = stage._load_cache_links(project)
+                mirrorlinks = stage._get_mirrorlinks(project)
+                (_, links_with_data, serial, _etag) = mirrorlinks._load_cache_links()
                 if links_with_data is None:
                     links_with_data = []
                 entrypath = entry.relpath
@@ -636,8 +637,9 @@ class Importer:
                     links.append((key, href))
                     requires_python.append(require_python)
                     yanked.append(is_yanked)
-                stage._save_cache_links(
-                    project, links, requires_python, yanked, serial, None)
+                mirrorlinks._save_cache_links(
+                    links, requires_python, yanked, serial, None
+                )
         elif filedesc["type"] == Rel.DocZip:
             version = filedesc["version"]
             # docs didn't always have entrymapping in export dump
