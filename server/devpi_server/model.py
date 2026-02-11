95025b259fcd68ae9173dcb1d73c19133170cc86
Florian Schulze <mail@florian-schulze.net>
2026-02-11 14:46:15 +0100

    Require hashes to be set when storing data.

--- server/devpi_server/model.py
+++ server/devpi_server/model.py
@@ -903,7 +903,7 @@ class BaseStage:
         content_or_file: ContentOrFile,
         *,
         filename: str | None = None,
-        hashes: Digests | None = None,
+        hashes: Digests,
         last_modified: str | None = None,
     ) -> ELink:
         if self.customizer.readonly:
@@ -1557,7 +1557,7 @@ class PrivateStage(BaseStage):
         filename: str,
         content_or_file: ContentOrFile,
         *,
-        hashes: Digests | None = None,
+        hashes: Digests,
         last_modified: str | None = None,
     ) -> ELink:
         if self.customizer.readonly:
@@ -1590,7 +1590,7 @@ class PrivateStage(BaseStage):
         version: str,
         content_or_file: ContentOrFile,
         *,
-        hashes: Digests | None = None,
+        hashes: Digests,
         last_modified: str | None = None,
     ) -> ELink:
         if self.customizer.readonly:
@@ -1889,7 +1889,7 @@ class LinkStore:
         basename: str,
         content_or_file: ContentOrFile,
         *,
-        hashes: Digests | None = None,
+        hashes: Digests,
         last_modified: str | None = None,
     ) -> ELink:
         overwrite = None
@@ -1918,7 +1918,7 @@ class LinkStore:
         for_entrypath: ELink | str | None,
         *,
         filename: str | None = None,
-        hashes: Digests | None = None,
+        hashes: Digests,
         last_modified: str | None = None,
     ) -> ELink:
         if isinstance(for_entrypath, ELink):
@@ -1986,7 +1986,7 @@ class LinkStore:
         basename,
         content_or_file,
         *,
-        hashes=None,
+        hashes,
         last_modified=None,
         ref_hash_spec=None,
     ):
