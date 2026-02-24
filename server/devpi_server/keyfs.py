f7d5a7fff8e544185d216ca3060288a45193b0a9
Florian Schulze <mail@florian-schulze.net>
2026-02-24 17:36:42 +0100

    Store index config in separate keyfs key.
    This reduces the amount of changes in the db as index changes are much more common than user changes and unrelated indexes aren't changed.

--- server/devpi_server/keyfs.py
+++ server/devpi_server/keyfs.py
@@ -187,12 +187,16 @@ class TxNotificationThread(Generic[Schema]):
                 self.keyfs.schema.USER,  # type: ignore[attr-defined]
             )(user=user)
             value = key.get_mutable()
+            key = cast(
+                "PTypedKey[dict, DictViewReadonly]",
+                self.keyfs.schema.INDEX,  # type: ignore[attr-defined]
+            )(user=user, index=index)
+            ixconfig = key.get_mutable()
         if not value:
             # the user doesn't exist anymore
             self._get_ixconfig_cache[cache_key] = None
             return None
-        ixconfig = value.get('indexes', {}).get(index)
-        if ixconfig is None:
+        if not ixconfig:
             # the index doesn't exist anymore
             self._get_ixconfig_cache[cache_key] = None
             return None
@@ -686,11 +690,11 @@ class TransactionRootModel(RootModel):
         super().delete_user(username)
 
     def delete_stage(self, username, index):
+        super().delete_stage(username, index)
         key = (username, index)
         if key in self.model_cache:
             assert self.model_cache[key] is not None
             del self.model_cache[key]
-        super().delete_stage(username, index)
 
     def get_user(self, name):
         if name not in self.model_cache:
