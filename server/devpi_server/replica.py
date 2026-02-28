8b6985c9f6ad6222b6e1734a5487b0a4d6736b28
Florian Schulze <mail@florian-schulze.net>
2026-02-28 18:25:02 +0100

    Move remote file iteration functions to mirror.py.

--- server/devpi_server/replica.py
+++ server/devpi_server/replica.py
@@ -21,10 +21,10 @@ from .markers import Absent
 from .markers import Deleted
 from .markers import absent
 from .markers import deleted
+from .mirror import FileStreamer
 from .model import UpstreamError
 from .normalized import normalize_name
 from .readonly import DictViewReadonly
-from .views import FileStreamer
 from .views import H_PRIMARY_UUID
 from devpi_common.types import cached_property
 from devpi_common.url import URL
