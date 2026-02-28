73dafc6dc8c7e97428984314c26f2aae5bcc9fa1
Florian Schulze <mail@florian-schulze.net>
2026-02-28 18:23:35 +0100

    Replace iter_relpaths_at with iter_keys_at_serial.

--- server/devpi_server/keyfs.py
+++ server/devpi_server/keyfs.py
@@ -49,6 +49,7 @@ import time
 
 if TYPE_CHECKING:
     from .interfaces import IIOFile
+    from .keyfs_types import IKeyFSKey
     from .keyfs_types import KeyFSTypesRO
     from .keyfs_types import KeyType
     from .keyfs_types import KeyTypeRO
@@ -800,8 +801,10 @@ class Transaction:
             self._model = TransactionRootModel(xom)
         return self._model
 
-    def iter_relpaths_at(self, typedkeys, at_serial):
-        return self.conn.iter_relpaths_at(typedkeys, at_serial)
+    def iter_keys_at_serial(
+        self, keys: Iterable[IKeyFSKey], at_serial: int
+    ) -> Iterator[KeyData]:
+        return self.conn.iter_keys_at_serial(keys, at_serial)
 
     def iter_serial_and_value_backwards(
         self, key: LocatedKey[KeyType, KeyTypeRO], last_serial: int
