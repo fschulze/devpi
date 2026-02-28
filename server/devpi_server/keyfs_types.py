0a7a02e41a8a338ea0f0fd564eba88a028cdefff
Florian Schulze <mail@florian-schulze.net>
2026-02-28 18:23:57 +0100

    Use new ULIDKey to encapsulate ULID info.

--- server/devpi_server/keyfs_types.py
+++ server/devpi_server/keyfs_types.py
@@ -2,7 +2,12 @@ from __future__ import annotations
 
 from .markers import Absent
 from .markers import Deleted
+from .markers import absent
 from .markers import deleted
+from .readonly import DictViewReadonly
+from .readonly import ListViewReadonly
+from .readonly import SetViewReadonly
+from .readonly import TupleViewReadonly
 from .readonly import ensure_deeply_readonly
 from .readonly import get_mutable_deepcopy
 from attrs import define
@@ -15,13 +20,11 @@ from typing import NewType
 from typing import TYPE_CHECKING
 from typing import TypeVar
 from typing import get_origin
-from zope.interface import Attribute
-from zope.interface import Interface
-from zope.interface import implementer
 import contextlib
 import random
 import re
 import time
+import weakref
 
 
 if TYPE_CHECKING:
@@ -30,34 +33,28 @@ if TYPE_CHECKING:
     from .interfaces import IWriter
     from .keyfs import KeyFS
     from .normalized import NormalizedName
-    from .readonly import DictViewReadonly
-    from .readonly import ListViewReadonly
-    from .readonly import SetViewReadonly
-    from .readonly import TupleViewReadonly
     from collections.abc import Callable
     from collections.abc import Iterator
     from pathlib import Path
     from re import Match
     from re import Pattern
     from typing import Self
-
-    KeyFSTypesRO = (
-        bool
-        | bytes
-        | DictViewReadonly
-        | float
-        | frozenset
-        | int
-        | ListViewReadonly
-        | SetViewReadonly
-        | str
-        | TupleViewReadonly
-    )
-    KeyFSTypes = (
-        bool | bytes | dict | float | frozenset | int | list | set | str | tuple
-    )
+    from typing import TypeGuard
 
 
+KeyFSTypesRO = (
+    bool
+    | bytes
+    | DictViewReadonly
+    | float
+    | frozenset
+    | int
+    | ListViewReadonly
+    | SetViewReadonly
+    | str
+    | TupleViewReadonly
+)
+KeyFSTypes = bool | bytes | dict | float | frozenset | int | list | set | str | tuple
 KeyType = TypeVar("KeyType")
 KeyTypeRO = TypeVar("KeyTypeRO")
 
@@ -93,11 +90,10 @@ class ULID(int):
 
 @frozen
 class Record(Generic[KeyType, KeyTypeRO]):
-    key: LocatedKey[KeyType, KeyTypeRO]
-    ulid: ULID
+    key: ULIDKey[KeyType, KeyTypeRO]
     value: KeyType | Deleted
     back_serial: int
-    old_ulid: ULID | Absent
+    old_key: ULIDKey[KeyType, KeyTypeRO] | Absent
     old_value: KeyTypeRO | Absent | Deleted
 
     def __attrs_post_init__(self) -> None:
@@ -116,20 +112,18 @@ RelPath = NewType("RelPath", str)
 
 
 @frozen
-class KeyData:
-    relpath: RelPath
-    keyname: str
-    ulid: ULID
+class KeyData(Generic[KeyType, KeyTypeRO]):
+    key: ULIDKey[KeyType, KeyTypeRO]
     serial: int
     back_serial: int
-    value: KeyFSTypesRO | Deleted
+    value: KeyTypeRO | Deleted
 
     @property
     def last_serial(self) -> int:
         return self.serial
 
     @property
-    def mutable_value(self) -> KeyFSTypes | Deleted:
+    def mutable_value(self) -> KeyType | Deleted:
         return deleted if (val := self.value) is deleted else get_mutable_deepcopy(val)
 
 
@@ -201,19 +195,14 @@ def iter_lineage(key: PatternedKey) -> Iterator[PatternedKey]:
         ancestor_key = ancestor_key.parent_key
 
 
-class IKeyFSKey(Interface):
-    key_name = Attribute("""
-        The name of the key type. """)
-
-
 H = TypeVar("H", bound=Hashable)
 
 
-@implementer(IKeyFSKey)
 class LocatedKey(Generic[KeyType, KeyTypeRO]):
-    __slots__ = ("key_name", "key_type", "keyfs", "location", "params")
+    __slots__ = ("_hash", "_keyfs", "key_name", "key_type", "location", "params")
+    _hash: int
+    _keyfs: weakref.ReferenceType[KeyFS]
     key_name: str
-    keyfs: KeyFS
     key_type: type[KeyType]
     location: str
     params: dict[str, str]
@@ -226,7 +215,7 @@ class LocatedKey(Generic[KeyType, KeyTypeRO]):
         key_type: type[KeyType],
         params: dict | None = None,
     ) -> None:
-        self.keyfs = keyfs
+        self._keyfs = weakref.ref(keyfs)
         self.key_name = key_name
         assert "{" not in location
         self.location = location
@@ -237,7 +226,10 @@ class LocatedKey(Generic[KeyType, KeyTypeRO]):
         self.params = {} if params is None else params
 
     def __hash__(self) -> int:
-        return hash((self.key_name, self.relpath))
+        _hash = getattr(self, "_hash", None)
+        if _hash is None:
+            _hash = self._hash = hash((self.key_name, self.relpath))
+        return _hash
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, LocatedKey):
@@ -256,8 +248,8 @@ class LocatedKey(Generic[KeyType, KeyTypeRO]):
     def exists(self) -> bool:
         return self.keyfs.tx.exists(self)
 
-    def get(self: LocatedKey[KeyType, KeyTypeRO]) -> KeyTypeRO:
-        return self.keyfs.tx.get(self)
+    def get(self, default: KeyType | KeyTypeRO | Absent = absent) -> KeyTypeRO:
+        return self.keyfs.tx.get(self, default=default)
 
     def get_mutable(self) -> KeyType:
         return self.keyfs.tx.get_mutable(self)
@@ -266,46 +258,69 @@ class LocatedKey(Generic[KeyType, KeyTypeRO]):
         return self.keyfs.tx.is_dirty(self)
 
     @property
-    def last_serial(self) -> int | None:
-        try:
-            return self.keyfs.tx.last_serial(self)
-        except KeyError:
-            return None
+    def back_serial(self) -> int:
+        return self.keyfs.tx.back_serial(self)
+
+    @property
+    def last_serial(self) -> int:
+        return self.keyfs.tx.last_serial(self)
+
+    def make_ulid_key(self, ulid: ULID) -> ULIDKey[KeyType, KeyTypeRO]:
+        return ULIDKey(
+            self.keyfs,
+            self.key_name,
+            self.location,
+            ulid,
+            self.key_type,
+            params=self.params,
+        )
+
+    def new_ulidkey(self) -> ULIDKey[KeyType, KeyTypeRO]:
+        ulid = self.keyfs._new_ulid()
+        return self.make_ulid_key(ulid)
 
     @property
     def relpath(self) -> RelPath:
         return RelPath(self.location)
 
+    @property
+    def keyfs(self) -> KeyFS:
+        keyfs = self._keyfs()
+        assert keyfs is not None
+        return keyfs
+
+    def resolve(self, *, fetch: bool) -> ULIDKey[KeyType, KeyTypeRO]:
+        return self.keyfs.tx.resolve(self, fetch=fetch)
+
     def set(self, val: KeyType) -> None:
-        if not isinstance(val, self.key_type) and not issubclass(
-            self.key_type, type(val)
-        ):
-            raise TypeError(
-                "%r requires value of type %r, got %r"
-                % (self.relpath, self.key_type.__name__, type(val).__name__)
-            )
         self.keyfs.tx.set(self, val)
 
     @contextlib.contextmanager
     def update(self) -> Iterator[KeyType]:
-        val = self.keyfs.tx.get_mutable(self)
+        keyfs = self._keyfs()
+        assert keyfs is not None
+        val = keyfs.tx.get_mutable(self)
+        assert isinstance(val, self.key_type)
         yield val
         # no exception, so we can set and thus mark dirty the object
-        self.set(val)
+        keyfs.tx.set(self, val)
 
 
-@implementer(IKeyFSKey)
 class PatternedKey(Generic[KeyType, KeyTypeRO]):
     __slots__ = (
+        "_keyfs",
         "_rex_reverse",
         "key_name",
         "key_type",
-        "keyfs",
         "parent_key",
         "pattern",
     )
+    _keyfs: weakref.ReferenceType[KeyFS]
     _rex_reverse: Pattern
+    key_name: str
     key_type: type[KeyType]
+    parent_key: PatternedKey | None
+    pattern: str
 
     def __init__(
         self,
@@ -315,7 +330,7 @@ class PatternedKey(Generic[KeyType, KeyTypeRO]):
         parent_key: PatternedKey | None,
         key_type: type[KeyType],
     ) -> None:
-        self.keyfs = keyfs
+        self._keyfs = weakref.ref(keyfs)
         self.key_name = key_name
         self.pattern = pattern
         self.parent_key = parent_key
@@ -329,7 +344,9 @@ class PatternedKey(Generic[KeyType, KeyTypeRO]):
             if "/" in val:
                 raise ValueError(val)
         location = self.full_pattern.format_map(kw)
-        return LocatedKey(self.keyfs, self.key_name, location, self.key_type, params=kw)
+        keyfs = self._keyfs()
+        assert keyfs is not None
+        return LocatedKey(keyfs, self.key_name, location, self.key_type, params=kw)
 
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} {self.key_name} {self.key_type.__name__} {self.pattern!r} {self.parent_key!r}>"
@@ -357,3 +374,98 @@ class PatternedKey(Generic[KeyType, KeyTypeRO]):
         if _rex_reverse is None:
             _rex_reverse = self._rex_reverse = get_rex_reverse(self.full_pattern)
         return _rex_reverse
+
+
+class ULIDKey(Generic[KeyType, KeyTypeRO]):
+    __slots__ = ("key_name", "key_type", "keyfs", "location", "params", "ulid")
+    keyfs: KeyFS
+    key_name: str
+    key_type: type[KeyType]
+    location: str
+    params: dict[str, str]
+    ulid: ULID
+
+    def __init__(
+        self,
+        keyfs: KeyFS,
+        key_name: str,
+        location: str,
+        ulid: ULID,
+        key_type: type[KeyType],
+        params: dict | None = None,
+    ) -> None:
+        self.keyfs = keyfs
+        self.key_name = key_name
+        assert "{" not in location
+        self.location = location
+        _key_type = get_origin(key_type)
+        if _key_type is None:
+            _key_type = key_type
+        self.key_type = _key_type
+        self.params = {} if params is None else params
+        self.ulid = ulid
+
+    def __hash__(self) -> int:
+        return hash(self.ulid)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ULIDKey):
+            return NotImplemented
+        return self.ulid == other.ulid
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.key_name} {self.key_type.__name__} {self.location!r} {self.ulid!r}>"
+
+    def delete(self) -> None:
+        return self.keyfs.tx.delete(self)
+
+    def exists(self) -> bool:
+        return self.keyfs.tx.exists(self)
+
+    def get(self, default: KeyType | KeyTypeRO | Absent = absent) -> KeyTypeRO:
+        return self.keyfs.tx.get(self, default=default)
+
+    def get_mutable(self) -> KeyType:
+        return self.keyfs.tx.get_mutable(self)
+
+    def is_dirty(self) -> bool:
+        return self.keyfs.tx.is_dirty(self)
+
+    @property
+    def back_serial(self) -> int:
+        return self.keyfs.tx.back_serial(self)
+
+    @property
+    def last_serial(self) -> int:
+        return self.keyfs.tx.last_serial(self)
+
+    def new_ulidkey(self) -> ULIDKey[KeyType, KeyTypeRO]:
+        ulid = self.keyfs._new_ulid()
+        return ULIDKey(
+            self.keyfs,
+            self.key_name,
+            self.location,
+            ulid,
+            self.key_type,
+            params=self.params,
+        )
+
+    @property
+    def relpath(self) -> RelPath:
+        return RelPath(self.location)
+
+    def set(self, val: KeyType) -> None:
+        self.keyfs.tx.set(self, val)
+
+    @contextlib.contextmanager
+    def update(self) -> Iterator[KeyType]:
+        val = self.get_mutable()
+        yield val
+        # no exception, so we can set and thus mark dirty the object
+        self.set(val)
+
+
+def is_dict_key(
+    key: LocatedKey | ULIDKey,
+) -> TypeGuard[LocatedKey[dict, DictViewReadonly] | ULIDKey[dict, DictViewReadonly]]:
+    return key.key_type is dict
