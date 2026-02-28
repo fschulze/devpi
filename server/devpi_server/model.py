d6d34b5ac19b8b117957638a75e5b5862aea28ea
Florian Schulze <mail@florian-schulze.net>
2026-02-28 18:23:57 +0100

    Use new ULIDKey to encapsulate ULID info.

--- server/devpi_server/model.py
+++ server/devpi_server/model.py
@@ -6,6 +6,7 @@ from .config import hookimpl
 from .filestore import Digests
 from .filestore import FileEntry
 from .keyfs_schema import KeyFSSchema
+from .keyfs_types import is_dict_key
 from .log import threadlog
 from .markers import Absent
 from .markers import Deleted
@@ -2432,7 +2433,7 @@ class EventSubscribers:
 
     def on_changed_version_config(self, ev: KeyChangeEvent) -> None:
         """ when version config is changed for a project in a stage"""
-        params = ev.key.params
+        params = ev.data.key.params
         keyfs = self.xom.keyfs
         hook = self.xom.config.hook
         with keyfs.read_transaction(at_serial=ev.at_serial) as tx:
@@ -2442,7 +2443,7 @@ class EventSubscribers:
             else:
                 assert ev.data.back_serial < ev.at_serial
                 try:
-                    old = tx.get_value_at(ev.key, ev.data.back_serial)
+                    old = tx.get_value_at(ev.data.key, ev.data.back_serial)
                 except KeyError:
                     old = {}
 
@@ -2462,7 +2463,7 @@ class EventSubscribers:
 
     def on_changed_file_entry(self, ev: KeyChangeEvent) -> None:
         """ when a file entry is modified. """
-        params = ev.key.params
+        params = ev.data.key.params
         user = params.get("user")
         index = params.get("index")
         keyfs = self.xom.keyfs
@@ -2470,13 +2471,13 @@ class EventSubscribers:
             stage = self.xom.model.getstage(user, index)
             if stage is not None and stage.ixconfig["type"] == "mirror":
                 return  # we don't trigger on file changes of pypi mirror
-            assert isinstance(ev.data.value, (Deleted, DictViewReadonly))
-            entry = FileEntry(ev.key, meta=ev.data.value)
+            assert is_dict_key(ev.data.key)
+            entry = FileEntry(ev.data.key, meta=ev.data.value)
             if not entry.project or not entry.version:
                 # the entry was deleted
                 self.xom.config.hook.devpiserver_on_remove_file(
                     stage=stage,
-                    relpath=ev.key.relpath,
+                    relpath=ev.data.key.relpath,
                 )
                 return
             name = entry.project
@@ -2491,7 +2492,7 @@ class EventSubscribers:
 
     def on_mirror_initialnames(self, ev: KeyChangeEvent) -> None:
         """ when projectnames are first loaded into a mirror. """
-        params = ev.key.params
+        params = ev.data.key.params
         user = params.get("user")
         index = params.get("index")
         keyfs = self.xom.keyfs
@@ -2505,14 +2506,14 @@ class EventSubscribers:
 
     def on_changed_index(self, ev: KeyChangeEvent) -> None:
         """when index data changes."""
-        params = ev.key.params
+        params = ev.data.key.params
         username = params.get("user")
         indexname = params.get("index")
         keyfs = self.xom.keyfs
         with keyfs.read_transaction(at_serial=ev.at_serial) as tx:
             if ev.data.back_serial > -1:
                 try:
-                    old = tx.get_value_at(ev.key, ev.data.back_serial)
+                    old = tx.get_value_at(ev.data.key, ev.data.back_serial)
                 except KeyError:
                     # the user was previously deleted
                     old = None
