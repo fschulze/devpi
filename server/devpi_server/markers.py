d8504e13d831b6302147265da40db54ddb004bb1
Florian Schulze <mail@florian-schulze.net>
2026-03-13 14:23:05 +0100

    Prevent markers from accidentally be used in boolean context.

--- server/devpi_server/markers.py
+++ server/devpi_server/markers.py
@@ -1,8 +1,18 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from typing import NoReturn
+
 
 class Absent:
     __slots__ = ()
+
+    def __bool__(self) -> NoReturn:
+        msg = f"{self} is no boolean"
+        raise RuntimeError(msg)
 
     def __repr__(self) -> str:
         return '<absent>'
@@ -14,6 +24,10 @@ absent = Absent()
 class Deleted:
     __slots__ = ()
 
+    def __bool__(self) -> NoReturn:
+        msg = f"{self} is no boolean"
+        raise RuntimeError(msg)
+
     def __repr__(self) -> str:
         return '<deleted>'
 
@@ -24,6 +38,10 @@ deleted = Deleted()
 class NoDefault:
     __slots__ = ()
 
+    def __bool__(self) -> NoReturn:
+        msg = f"{self} is no boolean"
+        raise RuntimeError(msg)
+
     def __repr__(self) -> str:
         return "<nodefault>"
 
@@ -33,6 +51,10 @@ nodefault = NoDefault()
 
 class NotSet:
     __slots__ = ()
+
+    def __bool__(self) -> NoReturn:
+        msg = f"{self} is no boolean"
+        raise RuntimeError(msg)
 
     def __repr__(self) -> str:
         return "<notset>"
