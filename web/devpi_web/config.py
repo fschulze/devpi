c17432afb58b95613966bfc88da758cb565965ca
Florian Schulze <mail@florian-schulze.net>
2026-03-27 07:29:16 +0100

    Fix some ruff/flake8 warnings in web.

--- web/devpi_web/config.py
+++ web/devpi_web/config.py
@@ -4,7 +4,7 @@ from pluggy import PluginManager
 
 def get_pluginmanager(config, load_entry_points=True):
     # lookup cached value
-    pm = getattr(config, 'devpiweb_pluginmanager', None)
+    pm = getattr(config, "devpiweb_pluginmanager", None)
     if pm is not None:
         return pm
     pm = PluginManager("devpiweb")
@@ -19,6 +19,10 @@ def get_pluginmanager(config, load_entry_points=True):
 
 def add_indexer_backend_option(parser, pluginmanager=None):
     parser.addoption(
-        "--indexer-backend", type=str, metavar="NAME", default="whoosh",
+        "--indexer-backend",
+        type=str,
+        metavar="NAME",
+        default="whoosh",
         action="store",
-        help="the indexer backend to use")
+        help="the indexer backend to use",
+    )
