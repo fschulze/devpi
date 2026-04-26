164090d7e1343fccadd4c80ef92cdd4977042b27
Florian Schulze <mail@florian-schulze.net>
2026-04-26 11:12:46 +0200

    Add some basic tests for devpi-debugging.

---
+++ debugging/tests/test_views.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import pytest
+
+
+pytestmark = [pytest.mark.notransaction]
+
+
+def test_keyfs_view(testapp):
+    r = testapp.get("/+keyfs", headers=dict(accept="text/html"))
+    html = BeautifulSoup(r.body, "html.parser")
+    (link,) = html.select("a")
+    assert link.text == "0"
+    assert "/+keyfs/0" in link.attrs["href"]
+
+
+def test_keyfs_changelog_view(testapp):
+    r = testapp.get("/+keyfs/0", headers=dict(accept="text/html"))
+    assert "<strong>USER root/.config</strong>" in r.text
