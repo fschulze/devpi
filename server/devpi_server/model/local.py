f538839891ff9df05ebe657e4128d98d3d5e6769
Florian Schulze <mail@florian-schulze.net>
2026-04-29 10:47:11 +0200

    Support "upload-time" for local indexes in JSON API version 1.1 response. Refs #1061

--- server/devpi_server/model/local.py
+++ server/devpi_server/model/local.py
@@ -604,6 +604,7 @@ class LocalIndex(BaseIndex):
                     relpath=v["relpath"],
                     require_python=v.get("requires_python"),
                     size=v["size"],
+                    upload_time=v["upload_time"],
                     user=username,
                     yanked=None,
                 )
@@ -669,6 +670,7 @@ class LocalIndex(BaseIndex):
             if rp := versiondata.get("requires_python"):
                 simpledata["requires_python"] = rp
             simpledata["size"] = link.entry.size
+            simpledata["upload_time"] = link.entry.last_modified
         return link
 
     def store_doczip(
