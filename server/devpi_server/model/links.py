0154fbfa65c44051dd096d99e4989e616f3da7a2
Florian Schulze <mail@florian-schulze.net>
2026-04-29 10:47:11 +0200

    Support "upload-time" for local indexes in JSON API version 1.1 response. Refs #1061

--- server/devpi_server/model/links.py
+++ server/devpi_server/model/links.py
@@ -501,6 +501,7 @@ class SimplelinkMeta:
         "relpath",
         "require_python",
         "size",
+        "upload_time",
         "user",
         "yanked",
     )
@@ -516,6 +517,7 @@ class SimplelinkMeta:
         relpath: RelPath,
         require_python: RequiresPython,
         size: int | None,
+        upload_time: str | None,
         user: str,
         yanked: Yanked,
     ) -> None:
@@ -530,6 +532,7 @@ class SimplelinkMeta:
         self.relpath = relpath
         self.require_python = require_python
         self.size = size
+        self.upload_time = upload_time
         self.user = user
         self.yanked = yanked
         if core_metadata and self.basename.endswith(".whl"):
